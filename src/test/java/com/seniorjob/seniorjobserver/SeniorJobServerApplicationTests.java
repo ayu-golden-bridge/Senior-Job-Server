d728728c502d0aefd1630327d0dd414ff05bf926
olive-su <1466su@naver.com>
2023-05-08 17:47:37 +0900

    Feat: Init Project

---
+++ src/test/java/com/seniorjob/seniorjobserver/SeniorJobServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniorjob.seniorjobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorJobServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
