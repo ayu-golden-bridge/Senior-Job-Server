176d7330caa03663b05480cd00cb19874e966b19
Arang <116303386+designAR@users.noreply.github.com>
2023-05-25 14:14:55 +0900

    Update LectureRepository.java
    열맞춤했습니다

--- src/main/java/com/seniorjob/seniorjobserver/repository/LectureRepository.java
+++ src/main/java/com/seniorjob/seniorjobserver/repository/LectureRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LectureRepository extends JpaRepository<LectureEntity, Long> {
-   List<LectureEntity> findByTitleContaining(String title);
+    List<LectureEntity> findByTitleContaining(String title);
 
     List<LectureEntity> findAllByOrderByCreatedDateDesc();
 
